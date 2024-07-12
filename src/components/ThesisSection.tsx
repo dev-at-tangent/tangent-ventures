377b30925ef1e15540e071696c2e1c32f475db1d
dev-at-tangent <mattchua2603@gmail.com>
2024-07-12 12:11:24 +0800

    highlight on scroll

--- src/components/ThesisSection.tsx
+++ src/components/ThesisSection.tsx
@@ -1,17 +1,67 @@
-import Lottie from "lottie-react";
-import data from "../assets/lotties/holo11.json";
+import { useRef, useEffect } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Balloon from "../assets/balloon.png";
 
-export default function ThesisSection() {
+gsap.registerPlugin(ScrollTrigger);
+
+export default function ThesisSection({
+  title,
+  text,
+  highlightedText,
+}: {
+  title: string;
+  text: string;
+  highlightedText: string;
+}) {
+  const ref = useRef(null);
+  const triggerRef = useRef(null);
+  useGSAP(
+    () => {
+      gsap.to(ref.current, {
+        backgroundPosition: "-99.99% 0",
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top 60%",
+          end: "bottom 60%",
+          scrub: true,
+        },
+      });
+    },
+    { scope: ref }
+  );
+
+  useEffect(() => {
+    // Refresh ScrollTrigger after a short delay to ensure all content is loaded
+    const refreshTimer = setTimeout(() => {
+      ScrollTrigger.refresh();
+    }, 100);
+
+    return () => clearTimeout(refreshTimer);
+  }, []);
+
   return (
-    <div className="">
-      <Lottie animationData={data} className="inset-0 z-0">
-        <p className="relative bottom-80 left-1/4 text-center w-1/2 z-10 text-grey-80">
-          At Tangent, we are keen to back founders building protocols and
-          applications that expand the on-chain economy. From first principles,
-          crypto has proven itself to be primarily good at three things besides
-          being money and being used for capital formation:
-        </p>
-      </Lottie>
+    <div className="flex flex-col items-center text-lg">
+      <img src={Balloon.src} alt="balloon" className="w-1/2" />
+
+      <h1 ref={triggerRef} className="text-5xl">
+        {title}
+      </h1>
+      <p className="text-center w-1/2 mt-8">
+        {text}
+        <span
+          ref={ref}
+          className="font-bold bg-gradient-to-r from-transparent from-50% via-transparent via-50% to-turq to-50%
+          bg-[-0%_0]
+          bg-[length:200%_auto]
+          transition-all
+          duration-500
+          ease-out"
+        >
+          {highlightedText}
+        </span>
+      </p>
     </div>
   );
 }
