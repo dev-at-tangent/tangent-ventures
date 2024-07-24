abc9bffc834402896183f0481e1f2ea2bc9b0ac9
dev-at-tangent <mattchua2603@gmail.com>
2024-07-24 17:00:20 +0800

    fixed height for endorsement card

--- src/components/EndorsementCard.tsx
+++ src/components/EndorsementCard.tsx
@@ -31,7 +31,7 @@ export default function EndorsementCard({
   return (
     <div
       className={`flex flex-col sm:flex-row items-start bg-white/60 backdrop-blur-sm drop-shadow-md rounded-xl p-8 desktop:p-12 group transition-colors sm:hover:bg-white ${
-        home ? "h-[450px] lg:h-[350px] mx-4" : "max-w-xl sm:min-h-[450px]"
+        home ? "h-[450px] 2xl:h-[350px] mx-4" : "max-w-xl sm:min-h-[450px]"
       }`}
     >
       <img
@@ -51,7 +51,6 @@ export default function EndorsementCard({
           details.fields.quote as unknown as Document,
           options
         )}
-        {/* <div className="grow bg-red-300" /> */}
         <div className="flex items-center mt-4 desktop:mt-12">
           <img
             src={(
