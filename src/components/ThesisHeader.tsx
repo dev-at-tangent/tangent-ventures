e027ba8029b26b877e24bdae4f2edae3aee16817
dev-at-tangent <mattchua2603@gmail.com>
2024-08-16 12:44:52 +0800

    added thesis cloud

--- src/components/ThesisHeader.tsx
+++ src/components/ThesisHeader.tsx
@@ -5,6 +5,7 @@ import { DotLottiePlayer } from "@dotlottie/react-player";
 
 import data from "../assets/lotties/thesis-holo.lottie";
 import mobiledata from "../assets/lotties/thesis-holo-mobile.lottie";
+import cloud from "../assets/lotties/thesis-cloud.lottie";
 
 import type { Entry } from "contentful";
 import type { ThesisHeaderContent } from "../pages/thesis.astro";
@@ -32,22 +33,30 @@ export default function ThesisHeader({
 
   return (
     <div
-      ref={ref}
-      className="flex items-center justify-center relative w-full "
-    >
-      <DotLottiePlayer src={data} autoplay loop className="hidden lg:flex" />
-      <DotLottiePlayer
-        src={mobiledata}
-        autoplay
-        loop
-        className="-mx-6 lg:hidden"
-      />
-      <div className="absolute top-0 flex flex-col sm:items-center justify-evenly w-full h-full py-8">
-        <h1 className="text-3xl sm:text-5xl glitch-text">THESIS</h1>
-        <p className=" text-sm md:text-base uhd:text-xl text-center mx-8 sm:w-1/2 z-10 text-grey-80 font-semibold">
-          {content.fields.content.toString()}
-        </p>
-      </div>
-    </div>
+  ref={ref}
+  className="flex items-center justify-center relative w-full"
+>
+  <DotLottiePlayer src={data} autoplay loop className="hidden lg:flex" />
+  <div className="absolute z-10 top-[80%] lg:top-1/2 right-0 lg:-right-32 overflow-hidden w-2/3 lg:w-auto -mx-6">
+    <DotLottiePlayer
+      src={cloud}
+      autoplay
+      loop
+      className="transform translate-x-12 lg:translate-x-0 w-full"
+    />
+  </div>
+  <DotLottiePlayer
+    src={mobiledata}
+    autoplay
+    loop
+    className="-mx-6 lg:hidden"
+  />
+  <div className="absolute top-0 flex flex-col sm:items-center justify-evenly w-full h-full py-8">
+    <h1 className="text-3xl sm:text-5xl glitch-text">THESIS</h1>
+    <p className="text-sm md:text-base uhd:text-xl text-center mx-8 sm:w-1/2 z-10 text-grey-80 font-semibold">
+      {content.fields.content.toString()}
+    </p>
+  </div>
+</div>
   );
 }
