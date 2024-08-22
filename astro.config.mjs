b1845dcfea2988b2947e435e9df913c26fcc978c
dev-at-tangent <mattchua2603@gmail.com>
2024-08-22 15:59:36 +0800

    added config for csr and ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    assetsInclude: ['**/*.lottie'],
+    assetsInclude: ['**/*.lottie']
   },
   integrations: [react(), tailwind()],
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
+  output: "hybrid",
+  adapter: vercel()
 });
