c2a2efa0e150d59be6681d9ff115ea3ba0106867
dev-at-tangent <mattchua2603@gmail.com>
2024-08-27 17:00:17 +0800

    added gtags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import partytown from '@astrojs/partytown'
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    assetsInclude: ['**/*.lottie'],
+    assetsInclude: ["**/*.lottie"],
   },
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   devToolbar: {
     enabled: false,
   },
