c64170d862de8a9209e14f76bf4e1b8012d935cb
dev-at-tangent <dev@tangent.ventures>
2026-02-02 15:45:42 +0800

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -93,7 +93,7 @@ export default function AboutSection({
             ref={firstTitleRef}
             className="md:w-1/2 text-3xl md:text-5xl font-medium text-center"
           >
-            WE PROVIDE HANDS-ON SUPPORT TO 10 FOUNDERS A YEAR
+            Capturing Inflection Points Across The Capital Stack
           </h1>
           <div
             ref={cardRef}
