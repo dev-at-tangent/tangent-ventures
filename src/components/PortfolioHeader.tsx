f79c2579b5f71c4af511d40521894acf37dc3567
dev-at-tangent <dev@tangent.ventures>
2026-02-02 14:48:49 +0800

    Update PortfolioHeader.tsx

--- src/components/PortfolioHeader.tsx
+++ src/components/PortfolioHeader.tsx
@@ -22,7 +22,7 @@ export default function PortfolioHeader() {
         ref={titleRef}
         className="text-3xl sm:text-5xl font-medium text-center"
       >
-        PORTFOLIO
+        VENTURE PORTFOLIO
       </h1>
       <DotLottiePlayer
         src={leftCloud}
